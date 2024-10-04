cafdb10341eda9ae76ef2ae9353afe2bf1fc9cbd
cornelber <bercornel@gmail.com>
2024-10-04 22:13:10 +0300

    add provider and path for country detail page based on cca2

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { Route, Routes } from "react-router-dom"
 import { Header } from "./components/Header"
 import Home from "./pages/Home"
-import Countries from "./pages/Countries"
+import CountriesPage from "./pages/CountriesPage"
+import CountryDetailsPage from "./pages/CountryDetailsPage"
+import { CountriesProvider } from "./context/CountriesContext"
 
 function App() {
-
   return (
-    <>
+    <CountriesProvider>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/countries" element={<Countries />} />
+        <Route path="/countries" element={<CountriesPage />} />
+        <Route path="/countries/:cca2" element={<CountryDetailsPage />} />
       </Routes>
-    </>
+    </CountriesProvider>
   )
 }
 
