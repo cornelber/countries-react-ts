a169ab849d88d46552427033a78eb8f88541c41b
cornelber <bercornel@gmail.com>
2024-10-05 21:44:58 +0300

    added not found page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import CountriesPage from "./pages/CountriesPage"
 import CountryDetailsPage from "./pages/CountryDetailsPage"
 import { CountriesProvider } from "./context/CountriesContext"
+import NotFoundPage from "./pages/NotFoundPage"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/countries" element={<CountriesPage />} />
         <Route path="/countries/:cca2" element={<CountryDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </CountriesProvider>
   )
