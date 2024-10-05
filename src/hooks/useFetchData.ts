3e665220a2ced7a51c7a5b20b6a78803fa46cdb4
cornelber <bercornel@gmail.com>
2024-10-05 21:42:56 +0300

    added error message component, updated err message in fetch hook, and added new prop to layout

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -10,12 +10,13 @@ export const useFetchData = <T>(apiUrl: string) => {
       try {
         const response = await fetch(apiUrl);
         if (!response.ok) {
-          throw new Error(`Failed to fetch data: ${response.statusText}`);
+          throw new Error(`Failed to fetch data. Server responded with status: ${response.status} - ${response.statusText}`);
         }
         const result = await response.json();
         setData(result);
       } catch (err) {
-        setError(`Failed to fetch data: ${(err as Error).message}`);
+        const errorMessage = (err as Error).message || "An unknown error occurred while fetching data.";
+        setError(errorMessage);
       } finally {
         setLoading(false);
       }
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import ErrorMessage from '../components/ErrorMessage';
 
 interface LayoutProps {
-    children: React.ReactNode
+    children: React.ReactNode,
+    error?: string | null;
 }
 
-const Layout = ({children} : LayoutProps) => {
+const Layout = ({children, error} : LayoutProps) => {
   return (
     <main className='my-12'>
+        {error && <ErrorMessage error={error} />}
         <div className="container mx-auto px-6">
             {children}
         </div>
