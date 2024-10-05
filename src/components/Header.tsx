6023b4e50218d359b3f185d63a8c342a6fa91e6f
cornelber <bercornel@gmail.com>
2024-10-05 21:41:10 +0300

    updated burger component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, useCallback, useMemo } from "react";
 import {
   Navbar,
-  MobileNav,
   Typography,
   Button,
   IconButton,
+  Collapse,
 } from "@material-tailwind/react";
 import { MoonIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
@@ -28,14 +28,14 @@ const NavItem = ({ href, children }: NavItemProps) => (
 );
 
 export function Header() {
-  const [openNav, setOpenNav] = useState(false);
+  const [openBurger, setOpenBurger] = useState(false);
 
   const handleMobileNav = useCallback(() => {
-    setOpenNav(!openNav);
-  }, [openNav]);
+    setOpenBurger(!openBurger);
+  }, [openBurger]);
 
   useEffect(() => {
-    const handleResize = () => window.innerWidth >= 960 && setOpenNav(false);
+    const handleResize = () => window.innerWidth >= 960 && setOpenBurger(false);
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
@@ -55,77 +55,66 @@ export function Header() {
       <Navbar className="mx-auto px-6 py-6 shadow-none rounded-none">
         <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
           <Link
-            to="#"
+            to="/"
             className="mr-4 cursor-pointer py-1.5 font-extrabold text-lg"
           >
             Where in the world?
           </Link>
           <div className="hidden lg:block">{navList}</div>
-          <div className="flex items-center gap-x-1">
-            <Button variant="text" size="sm" className="hidden lg:inline-flex">
+          <div className="lg:ml-0 ml-auto">
+            <Button variant="text" size="sm" className="inline-flex lg:py-2 lg:px-4 p-2">
               <MoonIcon className="h-5 w-5" />
               <Typography
                 variant="small"
-                className="font-medium ml-3 capitalize"
+                className="font-medium ml-3 capitalize lg:block hidden"
               >
                 Dark Mode
               </Typography>
             </Button>
+            <IconButton
+              variant="text"
+              className="ml-4 h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={handleMobileNav}
+            >
+              {openBurger ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
           </div>
-          <IconButton
-            variant="text"
-            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-            ripple={false}
-            onClick={handleMobileNav}
-          >
-            {openNav ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            )}
-          </IconButton>
         </div>
-        <MobileNav open={openNav}>
+        <Collapse open={openBurger}>
           <div className="container mx-auto">
             {navList}
-            <div className="flex items-center gap-x-1 mt-4">
-              <Button fullWidth variant="text" size="sm" className="flex">
-                <Typography
-                  variant="small"
-                  className="capitalize font-medium text-black mr-3"
-                >
-                  Dark Mode
-                </Typography>
-                <MoonIcon className="h-5 w-5" />
-              </Button>
-            </div>
           </div>
-        </MobileNav>
+        </Collapse>
       </Navbar>
     </header>
   );
