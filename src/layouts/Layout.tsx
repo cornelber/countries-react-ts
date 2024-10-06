88b2df8294ab575d4aab5846b0e1a9c25e93c294
cornelber <bercornel@gmail.com>
2024-10-06 15:31:43 +0300

    applied simple animation for main

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ErrorMessage from '../components/ErrorMessage';
 
 interface LayoutProps {
@@ -7,8 +7,17 @@ interface LayoutProps {
 }
 
 const Layout = ({children, error} : LayoutProps) => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+      setIsVisible(true);
+    }, []);
+
+  const slideUpClass = `transition-transform duration-700 ease-in-out translate-y-0 opacity-100`;
+  const hiddenClass = `translate-y-10 opacity-0`;
+
   return (
-    <main>
+    <main className={`${isVisible ? slideUpClass : hiddenClass}`}>
         {error && <ErrorMessage error={error} />}
         <div className="container mx-auto px-6 pt-36 pb-12">
             {children}
